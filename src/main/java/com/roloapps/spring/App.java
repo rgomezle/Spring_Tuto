c3d021e5c0351ed172465ef6328de41a48babba5
rgomezle <rgomez.leyton@gmail.com>
2017-08-22 12:46:10 -0300

    implementando inerfaz para flexibilidad

--- src/main/java/com/roloapps/spring/App.java
+++ src/main/java/com/roloapps/spring/App.java
@@ -7,6 +7,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.roloapps.beans.Ciudad;
 import com.roloapps.beans.Jugador;
 import com.roloapps.beans.Persona;
+import com.roloapps.interfaces.Equipo;
 
 public class App {
 
@@ -16,12 +17,14 @@ public class App {
 		ApplicationContext AppContext = 
 				new ClassPathXmlApplicationContext("com/roloapps/xml/beans.xml");
 		
-		Jugador jug = (Jugador) AppContext.getBean("messi");	
-		
-		System.out.println(jug.getNombre()+" - "+jug.getEquipo().mostrar());
+		//Jugador jug = (Jugador) AppContext.getBean("messi");	
+		//implementando la interfaz equipo, le da mas felxibilidad 
+		Equipo eq = (Equipo) AppContext.getBean("juventus");
+		Equipo eq2 = (Equipo) AppContext.getBean("barcelona");
+		//La interfaz para ambos equipos
+		System.out.println("Equipos: \n"+eq.mostrar()+"\n"+eq2.mostrar());
 			
 		((ConfigurableApplicationContext)AppContext).close();
-
 	}
 
 }
