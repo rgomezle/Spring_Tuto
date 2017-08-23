a5e04e59f08c89b1e1842c30bcdb599ed99ed083
rgomezle <rgomez.leyton@gmail.com>
2017-08-23 16:19:39 -0300

    Test XML - beans

--- src/main/java/com/roloapps/beans/Jugador.java
+++ src/main/java/com/roloapps/beans/Jugador.java
@@ -1,43 +1,39 @@
 package com.roloapps.beans;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Required;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
 import com.roloapps.interfaces.Equipo;
 
-@Component("messi")
 public class Jugador {
 
-	private int numero;
-	@Value("Lionel Messi")
-	private String nombre;
-	@Autowired //tiene byType por defecto
-	private Equipo equipo;
-	
-	public int getNumero() {
-		return numero;
-	}
-	public void setNumero(int numero) {
-		this.numero = numero;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public Equipo getEquipo() {
-		return equipo;
-	}
-	//@Required
-	public void setEquipo(Equipo equipo) {
-		this.equipo = equipo;
-	}
-	
-	
-	
-	
+private int id;
+private String nombre;
+private Equipo equipo;
+private Camiseta camiseta;
+
+
+
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+public Equipo getEquipo() {
+	return equipo;
+}
+public void setEquipo(Equipo equipo) {
+	this.equipo = equipo;
+}
+public Camiseta getCamiseta() {
+	return camiseta;
+}
+public void setCamiseta(Camiseta camiseta) {
+	this.camiseta = camiseta;
+}
+
 }
--- src/main/java/com/roloapps/spring/App.java
+++ src/main/java/com/roloapps/spring/App.java
@@ -1,20 +1,43 @@
 package com.roloapps.spring;
 
+import java.util.Scanner;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.roloapps.beans.Barcelona;
 import com.roloapps.beans.Jugador;
+import com.roloapps.beans.Juventus;
 
 public class App {
 
 	public static void main(String[] args) {
-				
+			
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Elija un equipo: 1-Barcelona 2-Juventus");
+		int respuesta = sc.nextInt();
+		
 		ApplicationContext AppContext = 
 				new ClassPathXmlApplicationContext("com/roloapps/xml/beans.xml");
-		
-		Jugador jug = (Jugador) AppContext.getBean("messi");	
+		Jugador jug = (Jugador) AppContext.getBean("jugador");	
+	
+		switch (respuesta) {
+		case 1:
+			jug.setEquipo(new Barcelona());
+			break;
 
-		System.out.println(jug.getNombre()+" - "+jug.getEquipo().mostrar());
+		case 2:
+			jug.setEquipo(new Juventus());
+			break;
+	
+		}
+		
+
+		System.out.println("Nombre: "+jug.getNombre()+"\nEquipo: "+jug.getEquipo().mostrar()+
+				"\nCamiseta: "+jug.getCamiseta().getMarca().getNombre()+
+				"\nNumero :"+jug.getCamiseta().getNumero());
 			
 		((ConfigurableApplicationContext)AppContext).close();
 	}
